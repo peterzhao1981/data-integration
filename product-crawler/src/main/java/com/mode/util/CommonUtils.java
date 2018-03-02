d7183965969fd5c74c6964af0cbdf2bca6a77d24
peterzhao1981 <z_w_w@hotmail.com>
2018-03-02 20:02:28 +0800

    Update exclude_purchase.txt

--- product-crawler/src/main/java/com/mode/util/CommonUtils.java
+++ product-crawler/src/main/java/com/mode/util/CommonUtils.java
@@ -29,8 +29,7 @@ public class CommonUtils {
 
     public static String skuConverter(String sku) {
         if (!org.springframework.util.StringUtils.isEmpty(sku)) {
-            sku = sku.replaceAll(" ", "");
-            sku = sku.replaceAll("&", "");
+            sku = sku.replaceAll(" ", "").replaceAll("&", "").replaceAll("\\+", "");
             if (sku.endsWith("#")) {
                 sku = sku.substring(0, sku.length() - 1);
             }
@@ -45,6 +44,6 @@ public class CommonUtils {
 //        System.out.println(parseProductName("Hair Claw Clip Straight Long Hair Ponytail Extensions - 2H33#"));
 //        System.out.println(parseProductName("Halloween Eyeball &Tai Chi Print Socks - Pattern-1 / US 6 - US 8.5"));
 
-        System.out.println(skuConverter("23413VF-F-Blue&Black&White "));
+        System.out.println(skuConverter("23413VF-F-Blu e&Black&+W++hite "));
     }
 }
